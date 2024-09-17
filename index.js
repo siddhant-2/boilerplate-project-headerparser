071d06fe45239592b41432c05b08981888330b60
siddhant-2 <sidsiddhant2@gmail.com>
2024-09-17 21:12:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] ||
+     req.socket.remoteAddress ||
+     null;
+  var language = req.headers["accept-language"]
+  var ua = req.headers['user-agent']
+  res.send({ "ipaddress": ip , "language":language, "software":ua});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
